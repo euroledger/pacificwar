ece3bfa9ac6df337b81a984d1cd184e3ace863d8
NB-MikeRichardson <mike.richardson@northernblock.io>
2022-05-24 09:04:36 +0300

    remove out folder
    Signed-off-by: NB-MikeRichardson <mike.richardson@northernblock.io>

--- out/src/scenarios/es1PearlHarbor/es1Config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.minNumberOfAirUnitTargets = exports.maxNumberOfAirUnitTargets = exports.numBattleshipsPerTargetGroup = void 0;
-/* Number of battleship targets in a group of targets for air units to attack on the first battle cycle */
-exports.numBattleshipsPerTargetGroup = 6;
-/* Max number Air Units to be attacked on battle cycle 1 */
-exports.maxNumberOfAirUnitTargets = 3;
-/* Minimum number of air units to attack on battle cycle 1 */
-exports.minNumberOfAirUnitTargets = 1;
